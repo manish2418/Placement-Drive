b9fea23c81e253ef6e2bf8c2b112bbe36856dbe0
Manish Kumar <mkdestiny2410@gmail.com>
2023-01-20 21:16:28 +0530

    Add files via upload

---
+++ LCM,GCD.cpp
@@ -0,0 +1,17 @@
+
+#include <bits/stdc++.h>
+
+using namespace std;
+int gcd(int n,int m){
+    return __gcd(n,m);
+}
+
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+    cout<<gcd(n,m);
+
+    return 0;
+}
